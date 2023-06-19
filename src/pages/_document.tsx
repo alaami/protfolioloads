aaabb95d94bdd2f7b28d932e0986e4fc8dfd2d82
alaamir <mir.alaa96@gmail.com>
2023-06-18 23:16:30 -0400

    Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,15 +11,32 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
         />
         <link
-         rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />     
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js"></script>
         <title>Fit IT Solutions - Canadian IT Consulting Company</title>
+         {/* Global Site Tag (gtag.js) - Google Analytics */}
+         <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       </Head>
-      <body style={{backgroundColor:customTheme.palette.neutral.main,color:customTheme.palette.neutral.contrastText}}>
+      <body style={{ backgroundColor: customTheme.palette.neutral.main, color: customTheme.palette.neutral.contrastText }}>
         <Main />
         <NextScript />
       </body>
